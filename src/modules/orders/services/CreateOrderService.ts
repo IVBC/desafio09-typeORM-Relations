7ff55bb5803fab63ab8fbffe2a4131d54d465d25
Isaque Vilson <ivbc.eng17@uea.edu.br>
2020-08-06 00:26:27 -0400

    finished

--- src/modules/orders/services/CreateOrderService.ts
+++ src/modules/orders/services/CreateOrderService.ts
@@ -20,13 +20,57 @@ interface IRequest {
 @injectable()
 class CreateOrderService {
   constructor(
+    @inject('OrdersRepository')
     private ordersRepository: IOrdersRepository,
+
+    @inject('ProductsRepository')
     private productsRepository: IProductsRepository,
+
+    @inject('CustomersRepository')
     private customersRepository: ICustomersRepository,
   ) {}
 
   public async execute({ customer_id, products }: IRequest): Promise<Order> {
-    // TODO
+    const customer = await this.customersRepository.findById(customer_id);
+
+    if (!customer) {
+      throw new AppError('Customer not found');
+    }
+
+    const productsFound = await this.productsRepository.findAllById(products);
+
+    if (productsFound.length !== products.length) {
+      throw new AppError('Product is missing');
+    }
+
+    const productsList = productsFound.map(product => {
+      const productList = products.find(p => p.id === product.id);
+
+      if (!productList) {
+        throw new AppError('Product not found');
+      }
+
+      if (product.quantity < productList.quantity) {
+        throw new AppError('Product out of stock');
+      }
+
+      return {
+        product_id: product.id,
+        price: product.price,
+        quantity: productList.quantity || 0,
+      };
+    });
+
+    const dataOrder = {
+      customer,
+      products: productsList,
+    };
+
+    await this.productsRepository.updateQuantity(products);
+
+    const order = await this.ordersRepository.create(dataOrder);
+
+    return order;
   }
 }
 
